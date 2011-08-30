1f1a2529b1656879080eaaf097f12572a854d593
y.kokubo <y.kokubo@otr-jp.com>
2011-08-30 15:08:54 +0900

    チケット保存に必要なフィールドを最低限うめた
    更新が必要なときだけ処理するようにした

--- lib/ical_import.rb
+++ lib/ical_import.rb
@@ -30,18 +30,31 @@ class IcalImporter
         start_date = start_date.new_offset(Rational(9, 24))
         end_date = event.dtend
         end_date = end_date.new_offset(Rational(9, 24))
-        puts "start   " + start_date.to_s
-        puts "end:    " + end_date.to_s
-        puts "uid:    " + event.uid.to_s
-        puts "summary:" + event.summary.to_s
-        puts "desc:   " + event.description.to_s
+        last_modified = event.last_modified
+        last_modified = last_modified.new_offset(Rational(9, 24))
 
-        issue = Issue.new
+        rel = UidRelation.find_by_uid event.uid
+        issue = rel.issue if rel
+        issue = Issue.new unless issue
+
+        next if issue and rel and rel.updated_at >= last_modified
+
+        issue.init_journal(User.find(3))
         issue.tracker_id = 39
         issue.subject = event.summary
         issue.description = event.description
         issue.project_id = 228
-        issue.save
+        issue.author_id = 3
+        issue.start_date = start_date
+        issue.due_date = end_date
+        issue.save!
+
+        unless rel
+          rel = UidRelation.new
+          rel.uid = event.uid
+          rel.issue = issue
+          rel.save!
+        end
       end
     end
   end
