dcfc4b2757e5bc5b8f8da94030d98d44da471465
y.kokubo <y.kokubo@otr-jp.com>
2011-09-01 14:16:16 +0900

    IcalSettingから読み込んで動くようにした
    loggerを埋め込んだ

--- lib/ical_import.rb
+++ lib/ical_import.rb
@@ -9,6 +9,7 @@ require 'date'
 require 'kconv'
 require 'net/https'
 require 'uri'
+require 'logger'
 
 include Icalendar
 
@@ -16,36 +17,46 @@ class IcalImporter
 
   class << self
     def import
+      logger = Logger.new(File.join(File.dirname(__FILE__), "../../../../log/ics_import.log"))
+      logger.level = Logger::DEBUG
+
       cals = nil
-      uri = URI.parse("hoge.ics")
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.start {
-      response = http.get(uri.request_uri)
-        cals = Icalendar::parse(response.body)
-      }
-      cal = cals[0]
-      cal.events.each do |event|
-        start_date = event.dtstart
-        start_date = start_date.new_offset(Rational(9, 24)) if start_date.respond_to? :new_offset and start_date.ical_params["TZID"].nil?
-        end_date = event.dtend
-        end_date = end_date.new_offset(Rational(9, 24)) if end_date.respond_to? :new_offset and end_date.ical_params["TZID"].nil?
-        last_modified = event.last_modified
-        last_modified = last_modified.new_offset(Rational(9, 24)) if last_modified and last_modified.ical_params["TZID"].nil?
+      IcalSetting.find(:all).each do |setting|
 
-        rel = UidRelation.find_by_uid event.uid
-        issue = rel.issue if rel
-        issue = Issue.new unless issue
+        next if setting.project.archived?
+        logger.info "ics import from #{setting.inspect}"
 
-        next if last_modified.nil? and not issue.new_record?
-        next if rel and rel.updated_at >= last_modified
+        uri = URI.parse(setting.url)
 
-        puts event.summary
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.start {
+        response = http.get(uri.request_uri)
+          cals = Icalendar::parse(response.body)
+        }
+        cal = cals[0]
+        cal.events.each do |event|
+          # MEMO: TZIDがなければ強制的に日本時間に強制しているけどほんとはVTIMEZONEを元に補正すべき
+          start_date = event.dtstart
+          start_date = start_date.new_offset(Rational(9, 24)) if start_date.respond_to? :new_offset and start_date.ical_params["TZID"].nil?
+          end_date = event.dtend
+          end_date = end_date.new_offset(Rational(9, 24)) if end_date.respond_to? :new_offset and end_date.ical_params["TZID"].nil?
+          last_modified = event.last_modified
+          last_modified = last_modified.new_offset(Rational(9, 24)) if last_modified and last_modified.ical_params["TZID"].nil?
 
-        issue.init_journal(User.find(3)) unless issue.new_record?
-        issue.tracker_id = 39
-        issue.subject = event.summary
-        issue.description = <<EOS
+          rel = UidRelation.find_by_uid event.uid
+          issue = rel.issue if rel
+          issue = Issue.new unless issue
+
+          next if last_modified.nil? and not issue.new_record?
+          next if rel and rel.updated_at >= last_modified
+
+          logger.info "create event #{event.inspect}"
+
+          issue.init_journal(setting.user) unless issue.new_record?
+          issue.tracker = setting.tracker
+          issue.subject = event.summary
+          issue.description = <<EOS
 * start:
 ** #{start_date.strftime("%Y-%m-%d %H:%M:%S")}
 * end:
@@ -55,19 +66,23 @@ class IcalImporter
 
 #{event.description}
 EOS
-        issue.project_id = 228
-        issue.author_id = 3
-        issue.start_date = start_date
-        issue.due_date = end_date
-        issue.save!
+          issue.project = setting.project
+          issue.author = setting.user
+          issue.start_date = start_date
+          issue.due_date = end_date
+          issue.save!
 
-        unless rel
-          rel = UidRelation.new
-          rel.uid = event.uid
-          rel.issue = issue
-          rel.save!
+          logger.info "saved issue : #{issue.inspect}"
+
+          unless rel
+            rel = UidRelation.new
+            rel.uid = event.uid
+            rel.issue = issue
+            rel.save!
+          end
         end
       end
+      logger.info "ics import finished."
     end
   end
 end
